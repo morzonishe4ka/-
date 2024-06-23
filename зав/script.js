03723d395022b9ff5b145587be8fbcc11bbe77a7
morzonishe4ka <levic9446@gmail.com>
2024-06-23 20:54:04 +0300

    Add files via upload

---
+++ зав/script.js
@@ -0,0 +1,50 @@
+// console.log(worker_1);
+// worker_1.style.opacity=0.5
+// console.log(worker_1_btn);
+// document.querySelector(".vacavaca").style.backgroundColor="red"
+worker_1_btn.onmouseover=function(){
+    worker_1.style.transform="translateX(540px)"
+    console.log(1234);
+}
+worker_1_btn.onmouseout=function(){
+    worker_1.style.transform="translateX(0px)"
+    console.log(14);
+}
+
+worker_2_btn.onmouseover=function(){
+    worker_2.style.transform="translateX(540px)"
+    console.log(1234);
+}
+worker_2_btn.onmouseout=function(){
+    worker_2.style.transform="translateX(0px)"
+    console.log(14);
+}
+
+worker_3_btn.onmouseover=function(){
+    worker_3.style.transform="translateX(540px)"
+    console.log(1234);
+}
+worker_3_btn.onmouseout=function(){
+    worker_3.style.transform="translateX(0px)"
+    console.log(14);
+}
+
+worker_4_btn.onmouseover=function(){
+    worker_4.style.transform="translateX(540px)"
+    console.log(1234);
+}
+worker_4_btn.onmouseout=function(){
+    worker_4.style.transform="translateX(0px)"
+    console.log(14);
+}
+
+worker_5_btn.onmouseover=function(){
+    worker_5.style.transform="translateX(540px)"
+    console.log(1234);
+}
+worker_5_btn.onmouseout=function(){
+    worker_5.style.transform="translateX(0px)"
+    console.log(14);
+}
+    // worker_1.style.transform="translateX(50px)"
+    
